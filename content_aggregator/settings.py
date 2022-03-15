5134ba5eb406545725e16b1392911b77307d1daa
ShuggyMcGuggy <mark.shury.smith@me.com>
2022-03-15 07:29:19 +0000

    fix missign comma in startjobs.py

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -170,8 +170,15 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     # Static asset configuration...
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    # STATIC_ROOT = 'staticfiles'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # # STATIC_ROOT = 'staticfiles'
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
                         )
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
