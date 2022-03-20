cf867a34b137a4e7e947c00a070ef83d9497eba9
ShuggyMcGuggy <mark.shury.smith@me.com>
2022-03-20 09:28:00 +0000

    Now have images on the publiction page

--- podcasts/models.py
+++ podcasts/models.py
@@ -58,6 +58,8 @@ class NewsItem(models.Model):
 class Publication(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.URLField(default="https://www.clipartmax.com/png/middle/265-2655834_work-in-progress-icon.png")
+    image_file = models.CharField(max_length=20, default="wip.jpg") # file name of the image file held in staticfiles/img
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
