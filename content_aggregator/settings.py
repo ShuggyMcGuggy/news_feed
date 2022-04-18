909d6411d69e58018f32f8c3cc897982d68c07aa
ShuggyMcGuggy <mark.shury.smith@me.com>
2022-04-18 10:20:37 +0100

    static ref

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -193,8 +193,8 @@ if os.getcwd() == '/app':
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     # STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = 'staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+    STATIC_URL = '/staticfiles/'
 
     STATICFILES_DIRS = (
         # os.path.join(BASE_DIR, 'static'),
