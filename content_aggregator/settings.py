35acda68b12965f0dd62e502d08fc9de541dc679
ShuggyMcGuggy <mark.shury.smith@me.com>
2022-04-18 11:38:21 +0100

    static links

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -129,12 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-BASE_DIR / "content_aggregator/static",
-
-]
+STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = "static/"
+# STATICFILES_DIRS = [
+# BASE_DIR / "content_aggregator/static",
+#
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -191,11 +192,14 @@ if os.getcwd() == '/app':
 
 
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    # STATIC_ROOT = BASE_DIR / 'staticfiles'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = 'staticfiles/'
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # # STATIC_ROOT = BASE_DIR / 'staticfiles'
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_URL = 'staticfiles/'
+    #
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    STATIC_URL = "static/"
+    STATIC_ROOT = "static/"
