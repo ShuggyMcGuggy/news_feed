e5112b9ee0ec18368049fc4a40f4a722f7949fe4
ShuggyMcGuggy <mark.shury.smith@me.com>
2022-12-30 07:35:39 +0000

    Fixed the PREv and NEXt Page bug
    Forgot that the change of status of the new news story from NEW takes it out of the view.
    
    Need to use the change URL to pass the PREV and NEXT news references.

--- podcasts/views.py
+++ podcasts/views.py
@@ -426,12 +426,6 @@ def edit_news_item_links(request, news_item_id='1'):
     # print(l_final)
 
 
-
-
-
-
-
-
     if request.method != 'POST':
         # Initial request; pre-fill the form with the current entry
         form = NewsItemForm(instance=news_item, prefix='news')
@@ -449,7 +443,9 @@ def edit_news_item_links(request, news_item_id='1'):
             print(request.POST)
             # new_story_mapping = pub_link_form.save(commit=False)
             pub_link_form.save()
-            return HttpResponseRedirect(reverse('podcasts:edit_news_item_links', args=[news_item.id]))
+            # return HttpResponseRedirect(reverse('podcasts:edit_news_item_links', args=[news_item.id]))
+            return HttpResponseRedirect(reverse('podcasts:edit_news_links_changed',
+                                            args=[news_item.id, prev_news_id, next_news_id]))
 
 
     context = {'news_item': news_item,
@@ -470,6 +466,36 @@ def edit_news_item_links(request, news_item_id='1'):
 
 @login_required
 def edit_news_links_changed(request, news_item_id='1', prev_news_id='1', next_news_id='1'):
+    # """ Edit an existing news item"""
+    # news_item = NewsItem.objects.get(id=news_item_id)
+    #
+    # # filter the list by status of 3 == NEW
+    # status_new = Status.objects.filter(state='New')
+    # l_news_items = NewsItem.objects.filter(status=status_new[0].id).order_by("-pub_date")
+    #
+    # if request.method != 'POST':
+    #     # Initial request; pre-fill the form with the current entry
+    #     form = NewsItemForm(instance=news_item, prefix='news')
+    #     link_form = NewsArticleMapForm(prefix='link')
+    #     pub_link_form = NewsLinkPubForm(prefix='PubLink')
+    # else:
+    #     # POST data submitted; process data
+    #     form = NewsItemForm(instance=news_item, data=request.POST, prefix='news')
+    #     link_form = NewsArticleMapForm(request.POST, prefix='link')
+    #     pub_link_form = NewsLinkPubForm(data=request.POST, prefix='Publink')
+    #
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(reverse('podcasts:edit_news_links_changed') + '/' + news_item_id + '/' + prev_news_id + '/' + next_news_id)
+    #
+    # context = {'news_item': news_item,
+    #            'form': form,
+    #            'link_form': link_form,
+    #            'pub_link_form': pub_link_form,
+    #            'prev_news_id': prev_news_id,
+    #            'next_news_id': next_news_id}
+    # return render(request, 'edit_news_item_links.html', context)
+
     """ Edit an existing news item"""
     news_item = NewsItem.objects.get(id=news_item_id)
 
@@ -477,25 +503,69 @@ def edit_news_links_changed(request, news_item_id='1', prev_news_id='1', next_ne
     status_new = Status.objects.filter(state='New')
     l_news_items = NewsItem.objects.filter(status=status_new[0].id).order_by("-pub_date")
 
+    # Find the position matching ID in the list of news items
+    # Confirm the length of the list of items
+    # If there are more than 3 items in the list, set prev and next as one before an one after
+
+    counter = 0
+    for news_object in l_news_items:
+        # print("list includes:" +str( news_object.id) + " Looking for: " + str(news_item_id))
+        if str(news_object.id) == str(news_item_id):
+            if counter > 0:
+                prev_news_id = l_news_items[counter - 1].id
+            else:
+                prev_news_id = news_item_id
+            if counter < (len(l_news_items) - 1):
+                next_news_id = l_news_items[counter + 1].id
+            else:
+                next_news_id = news_item_id
+        counter = counter + 1
+
+    # Get list of all the linked items for this new item
+    l_links_to_pubs = Publication_Stories.objects.filter(news_item_id=news_item_id)
+    l_linked_pubs = []
+    for plink in l_links_to_pubs:
+        linked_pub = plink.publication_id
+        if linked_pub is not None:
+            l_linked_pubs = l_linked_pubs + [linked_pub]
+
+    # Get list of all publications
+    l_pubs = Publication.objects.all()
+
+    # Create a list of all publications not linked to the news item.
+    # This is a list of all publicatoin minus the list of items currently linked
+    l_non_linked_pubs = []
+    for plink in l_pubs:
+        if plink not in l_linked_pubs:
+            l_non_linked_pubs.append(plink)
+
     if request.method != 'POST':
         # Initial request; pre-fill the form with the current entry
         form = NewsItemForm(instance=news_item, prefix='news')
         link_form = NewsArticleMapForm(prefix='link')
-        pub_link_form = NewsLinkPubForm(prefix='PubLink')
+        pub_link_form = NewsLinkPubForm(prefix='PubLink', initial={'news_item_id': news_item_id})
+
+
     else:
         # POST data submitted; process data
         form = NewsItemForm(instance=news_item, data=request.POST, prefix='news')
-        link_form = NewsArticleMapForm(request.POST, prefix='link')
-        pub_link_form = NewsLinkPubForm(data=request.POST, prefix='Publink')
-
-        if form.is_valid():
+        pub_link_form = NewsLinkPubForm(data=request.POST, prefix='PubLink')
+        if form.is_valid() and pub_link_form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('podcasts:edit_news_links_changed') + '/' + news_item_id + '/' + prev_news_id + '/' + next_news_id)
+            print("from IS valid")
+            print(request.POST)
+            # new_story_mapping = pub_link_form.save(commit=False)
+            pub_link_form.save()
+            # return HttpResponseRedirect(reverse('podcasts:edit_news_item_links', args=[news_item.id]))
+            return HttpResponseRedirect(reverse('podcasts:edit_news_links_changed',
+                                            args=[news_item.id, prev_news_id, next_news_id]))
 
     context = {'news_item': news_item,
                'form': form,
-               'link_form': link_form,
                'pub_link_form': pub_link_form,
+               'l_linked_pubs': l_linked_pubs,
+               'l_non_linked_pubs': l_non_linked_pubs,
+               'l_links_pubs': l_links_to_pubs,
                'prev_news_id': prev_news_id,
                'next_news_id': next_news_id}
     return render(request, 'edit_news_item_links.html', context)
