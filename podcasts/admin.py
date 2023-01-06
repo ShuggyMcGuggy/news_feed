039b0fe7dd2f71e8577fb5c7078f5cb2e660d186
ShuggyMcGuggy <mark.shury.smith@me.com>
2023-01-06 10:01:48 +0000

    commented out import export
    Commented out the import of the ImportExport module

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
+# from import_export import resources
+# from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Episode, NewsItem, Status, Publication, Publication_Stories, PageExport, RSS_feed
